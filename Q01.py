8c4af9039498d031c17e128b18009437cd3158d0
brunofecs <108473946+brunofecs@users.noreply.github.com>
2023-09-08 11:25:03 -0300

    Update Q01.py

--- Q01.py
+++ Q01.py
@@ -4,10 +4,19 @@ import random
 arq = open('Lista_nao_ordenada.txt', 'w')
 lista = []
 i=1
-n = int(input("Insira o número de elementos a serem gerados: "))
-while i <= n:
-    lista.append(random.randint(1,1000000))
-    i += 1
-lista_str = str(lista).strip('[]')
-arq.write(lista_str)
+
+try:
+    n = int(input("Insira o número de elementos a serem gerados: "))
+    if n > 0:
+        while i <= n:
+            lista.append(random.randint(1,1000000))
+            i += 1
+        lista_str = str(lista).strip('[]')
+        print('Escrevendo no arquivo...')
+        arq.write(lista_str)
+    else:
+        print('Informe um valor maior que 0.')
+except:
+    print('Informe um valor númerico.')
+
 arq.close()
